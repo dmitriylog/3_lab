eaa91159f4a7a1662baaacb40d8b6af4c73420e6
d1m1d1n <dmitriy-loginov-02@mail.ru>
2025-10-08 16:47:33 +0300

    Описание продуктов и категорий продуктов + геттеры и сеттеры

--- 3_lab/main.cpp
+++ 3_lab/main.cpp
@@ -18,6 +18,7 @@ public:
         return name;
     }
 
+    // геттеры
     double getPrice() const{
         return price;
     }
@@ -26,6 +27,7 @@ public:
         return rating;
     }
 
+    // сеттеры
     void setName(const string& newName){
         name = newName;
     }
@@ -52,6 +54,43 @@ public:
     Category(const string& catName):name(catName){}
     Category(const string& catName, const vector <Goods>& initGoods): name(catName),goods(initGoods){}
 
+    //геттеры
+    string getName() const{
+        return name;
+    }
+
+    const vector <Goods>& getGoods() const {
+        return goods;
+    }
+
+    // сеттеры
+    void setName(const string& newName){
+        name = newName;
+    }
+
+    void addGoods(const Goods& product){
+        goods.push_back(product);
+    }
+
+    void removeGoods(const string& productName){
+        for (auto it = goods.begin();it != goods.end(); ++it){
+            if (it->getName() == productName){
+                goods.erase(it);
+                break;
+            }
+        }
+    }
+
+    void diplayInfo() const {
+        cout << "Категория: " << name << endl;
+        cout << "Количество товаров: " << goods.size() << endl;
+        cout << "Товары: " << endl;
+
+        for (size_t i = 0; i < goods.size(); ++i){
+            cout << " " << (i+1) << ". ";
+            goods[i].displayInfo();
+        }
+    }
 };
 
 int main()
